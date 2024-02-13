8b7934af399f375db83dba7dfcfe9d45fd0c22d5
tusharkumar07 <tusharkumar0510@gmail.com>
2024-02-13 11:50:41 +0530

    mongodb atlas setup with backend

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const Table=require('./database/entry');
 
 
-mongoose.connect('mongodb+srv://tusharkumar0510:MI4XsFcNiyuO6GIz@issuebooks.2xwsdza.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+mongoose.connect('mongodb+srv://tusharkumar0510:MI4XsFcNiyuO6GIz@issuebooks.2xwsdza.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(()=>{
     console.log("Connected with DataBase");
 }).catch((err)=>{
     console.log(`Error in connecting Dtabase : ${err}`);
